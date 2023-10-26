44b5cc6d808afe85aa91ff7f09390a6fa6a480bb
Alex Dee <92275256+han73r@users.noreply.github.com>
2023-10-26 23:24:02 +0300

    Time: 63 ms (80.63%), Space: 38.1 MB (40.96%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.cs
+++ 0020-valid-parentheses/0020-valid-parentheses.cs
@@ -2,41 +2,37 @@ public class Solution
 {
     public bool IsValid(string s) 
     {
-        if (s.Length % 2 != 0)
+        int length = s.Length;
+        if (length % 2 != 0)              // Check length 
         {
             return false;
-        }
+        }   
 
-        Stack<char> stack = new Stack<char>();
+        char[] stack = new char[length];
+        int top = -1;                       // index top of stack
 
         foreach (char c in s)
         {
             if (c == '(' || c == '[' || c == '{')
             {
-                stack.Push(c);
+                stack[++top] = c;           // Add to stack
             }
             else
             {
-                if (stack.Count == 0)
+                if (top == -1 || !IsValidPair(stack[top], c))
                 {
                     return false;
                 }
-                char top = stack.Pop();
-                if (c == ')' && top != '(')
-                {
-                    return false;
-                }
-                else if (c == ']' && top != '[')
-                {
-                    return false;
-                }
-                else if (c == '}' && top != '{')
-                {
-                    return false;
-                }
+                top--;                      // Delete from stack
             }
         }
+        return top == -1;                   // stack should be empty
+    }
 
-        return stack.Count == 0;
+    private bool IsValidPair(char open, char close)
+    {
+        return (open == '(' && close == ')') ||
+               (open == '[' && close == ']') ||
+               (open == '{' && close == '}');
     }
 }
