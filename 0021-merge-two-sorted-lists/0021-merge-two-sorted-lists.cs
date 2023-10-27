bcb7c52abd3971f062ebdb805426f4df82a42e57
Alex Dee <92275256+han73r@users.noreply.github.com>
2023-10-27 22:08:35 +0300

    Time: 72 ms (92.08%), Space: 39.7 MB (88.45%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cs
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cs
@@ -16,25 +16,44 @@ public class Solution
 {
     public ListNode MergeTwoLists(ListNode list1, ListNode list2)
     {
-        ListNode dummy = new ListNode();
-        ListNode current = dummy;
+        if (list1 == null) return list2;
+        if (list2 == null) return list1;
 
-        while (list1 != null && list2 != null)
+        if (list1.val < list2.val)
         {
-            if (list1.val < list2.val)
-            {
-                current.next = list1;
-                list1 = list1.next;
-            }
-            else
-            {
-                current.next = list2;
-                list2 = list2.next;
-            }
-            current = current.next;
+            list1.next = MergeTwoLists(list1.next, list2);
+            return list1;
         }
-        current.next = list1 ?? list2;
+        else
+        {
+            list2.next = MergeTwoLists(list1, list2.next);
+            return list2;
+        }
+        // ListNode dummy = new ListNode(-1);
+        // ListNode current = dummy;
 
-        return dummy.next;
+        // while (list1 != null && list2 != null)
+        // {
+        //     if (list1.val <= list2.val)
+        //     {
+        //         current.next = list1;
+        //         list1 = list1.next;
+        //     }
+        //     else
+        //     {
+        //         current.next = list2;
+        //         list2 = list2.next;
+        //     }
+        //     current = current.next;
+        // }    
+        // if (list1 != null) 
+        // {
+        //     current.next = list1;
+        // } 
+        // else 
+        // {
+        //     current.next = list2;
+        // }
+        // return dummy.next;
     }
 }
