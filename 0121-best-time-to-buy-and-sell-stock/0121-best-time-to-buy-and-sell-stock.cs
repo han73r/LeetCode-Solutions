64ff14b77ff2491763dff817a9e567b3ac7314a8
Alex Dee <92275256+han73r@users.noreply.github.com>
2023-10-28 17:09:45 +0300

    Time: 221 ms (27.14%), Space: 50 MB (60.54%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cs
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cs
@@ -2,28 +2,14 @@ public class Solution
 {
     public int MaxProfit(int[] prices) 
     {
-        int pricesSize = prices.Length;
-        if (pricesSize == 1) { return 0; }
-        if (pricesSize == 2)
-        {
-            return prices[1] - prices[0] > 0 ? prices[1] - prices[0] : 0;
-        }
-
-        int maxProfit = 0;
         int minPrice = int.MaxValue;
-
-        for (int i = 0; i < prices.Length; i++)
+        int maxProfit = 0;
+        
+        for (int i = 0; i < prices.Length; i++) 
         {
-            if (prices[i] < minPrice)
-            {
-                minPrice = prices[i];
-            }
-            else if (prices[i] - minPrice > maxProfit)
-            {
-                maxProfit = prices[i] - minPrice;
-            }
-        }
-
+            minPrice = Math.Min(minPrice, prices[i]);
+            maxProfit = Math.Max(maxProfit, prices[i] - minPrice);
+        }       
         return maxProfit;
     }
 }
