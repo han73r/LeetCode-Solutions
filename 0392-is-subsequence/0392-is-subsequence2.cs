58a8135557ce82d619943a46cce81beeb88f3d86
Alex Dee <92275256+han73r@users.noreply.github.com>
2023-11-01 09:31:24 +0300

    Time: 72 ms (89.60%), Space: 37.82 MB (49.98%) - LeetHub

---
+++ 0392-is-subsequence/0392-is-subsequence2.cs
@@ -0,0 +1,24 @@
+public class Solution 
+{
+    public bool IsSubsequence(string s, string t) 
+    {
+        int j = 0;
+
+        if (s.Length == 0)
+        {
+            return true;
+        }
+
+        for (int i = 0; i < t.Length; i++)
+        {
+            if (s[j] == t[i])
+            {                 
+                if (++j == s.Length)
+                {
+                    return true;
+                }
+            }
+        }
+        return false;        
+    }
+}
