206f0646e106b27685f3c4c63b7ca502892b6423
Alex Dee <92275256+han73r@users.noreply.github.com>
2023-11-07 20:43:16 +0300

    Time: 197 ms (68%), Space: 56.7 MB (56%) - LeetHub

---
+++ 2049-eliminate-maximum-number-of-monsters/2049-eliminate-maximum-number-of-monsters.cs
@@ -0,0 +1,24 @@
+public class Solution 
+{
+    public int EliminateMaximum(int[] dist, int[] speed) 
+    {
+        int n = dist.Length;
+        int[] arrivalTime = new int[n];
+
+        for (int i = 0; i < n; i++)
+        {
+            arrivalTime[i] = (dist[i] - 1) / speed[i];
+        }
+
+        Array.Sort(arrivalTime);
+
+        for (int i = 0; i < n; i++)
+        {
+            if (i > arrivalTime[i])
+            {
+                return i;
+            }
+        }
+        return n;
+    }
+}
