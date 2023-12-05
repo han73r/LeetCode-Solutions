a575b3f84eccd93dd7375d19956beffa4d8c062f
Alex Dee <han73r@gmail.com>
2023-12-05 19:25:56 +0300

    Time: 24 ms (31.65%), Space: 26.77 MB (13.92%) - LeetHub

---
+++ 1688-count-of-matches-in-tournament/1688-count-of-matches-in-tournament.cs
@@ -0,0 +1,21 @@
+public class Solution
+{
+    public int NumberOfMatches(int n)
+    {
+        int matchesPlayed = 0;
+        while (n > 1)
+        {
+            if (n % 2 == 0)
+            {
+                matchesPlayed += n / 2;
+                n /= 2;
+            }
+            else
+            {
+                matchesPlayed += (n - 1) / 2;
+                n = (n - 1) / 2 + 1;
+            }
+        }
+        return matchesPlayed;
+    }
+}
