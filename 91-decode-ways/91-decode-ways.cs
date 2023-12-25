e9555dc346c14bbb626f175ca839f4aa7ff7c713
Alex Dee <han73r@gmail.com>
2023-12-25 16:44:31 +0530

    Time: 48 ms (93.60%), Space: 37.48 MB (19.99%) - LeetHub

---
+++ 91-decode-ways/91-decode-ways.cs
@@ -0,0 +1,27 @@
+public class Solution
+{
+    public int NumDecodings(string s)
+    {
+        if (s[0] == '0') return 0;
+
+        int n = s.Length;
+        int[] dp = new int[n + 1];
+        dp[0] = 1;
+        dp[1] = s[0] != '0' ? 1 : 0;
+
+        for (int i = 2; i <= n; i++)
+        {
+            int oneDigit = int.Parse(s.Substring(i - 1, 1));
+            int twoDigits = int.Parse(s.Substring(i - 2, 2));
+            if (oneDigit >= 1)
+            {
+                dp[i] += dp[i - 1];
+            }
+            if (twoDigits >= 10 && twoDigits <= 26)
+            {
+                dp[i] += dp[i - 2];
+            }
+        }
+        return dp[n];
+    }
+}
