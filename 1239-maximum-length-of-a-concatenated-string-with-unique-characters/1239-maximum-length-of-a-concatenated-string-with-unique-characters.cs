622c23b383549440dfe4dca508d7a49a9088b15a
Alex Dee <han73r@gmail.com>
2024-01-23 20:59:52 +0300

    Time: 96 ms (55.00%), Space: 122.07 MB (6.00%) - LeetHub

---
+++ 1239-maximum-length-of-a-concatenated-string-with-unique-characters/1239-maximum-length-of-a-concatenated-string-with-unique-characters.cs
@@ -0,0 +1,18 @@
+using System.Collections.Generic;
+using System.Linq;
+using System;
+
+public class Solution
+{
+    public int MaxLength(IList<string> arr, int i = 0, string s = "")
+    {
+        if (s.Distinct().Count() < s.Length) return 0;
+
+        if (arr.Count == i) return s.Length;
+
+        return Math.Max(
+            MaxLength(arr, i + 1, s),
+            MaxLength(arr, i + 1, s + arr[i])
+        );
+    }
+}
