238f36fc716f0d6b806a3d412be7fb79bc92a136
Alex Dee <han73r@gmail.com>
2024-03-02 19:09:58 +0300

    Time: 139 ms (95.87%), Space: 62.72 MB (91.59%) - LeetHub

---
+++ 977-squares-of-a-sorted-array/977-squares-of-a-sorted-array.cs
@@ -0,0 +1,11 @@
+using System;
+
+public class Solution {
+    public int[] SortedSquares(int[] nums) {
+        for (int i = 0; i < nums.Length; i++) {
+            nums[i] *= nums[i];
+        }
+        Array.Sort(nums);
+        return nums;
+    }
+}
