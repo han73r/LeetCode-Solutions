1a818155a72244126177742b5c132dda3d6caf14
Alex Dee <han73r@gmail.com>
2024-05-11 19:41:43 +0300

    Update 857-minimum-cost-to-hire-k-workers.cs

--- 857-minimum-cost-to-hire-k-workers.cs/857-minimum-cost-to-hire-k-workers.cs
+++ 857-minimum-cost-to-hire-k-workers.cs/857-minimum-cost-to-hire-k-workers.cs
@@ -1,18 +1,28 @@
-// less memory
-public class Solution
-{
-    public int[] TwoSum(int[] nums, int target)
-    {
-    for (int i = 0; i < nums.Length; i++)
-    {
-        for (int j = i + 1; j < nums.Length; j++)
-        {
-            if (nums[i] + nums[j] == target)
-            {
-                return new int[] { i, j };
+using System.Collections.Generic;
+using System;
+
+public class Solution {
+    public double MincostToHireWorkers(int[] quality, int[] wage, int k) {
+        List<(int, double)> ratio = new List<(int, double)>();
+        int len = quality.Length;
+        for (int i = 0; i < len; i++) {
+            ratio.Add((quality[i], (double)wage[i] / (double)quality[i]));
+        }
+
+        ratio = ratio.OrderBy(x => x.Item2).ToList();
+
+        int totQuantity = 0;
+        double result = 0;
+        PriorityQueue<int, int> heap = new PriorityQueue<int, int>();
+        for (int i = 0; i < len; i++) {
+            heap.Enqueue(ratio[i].Item1, -1 * ratio[i].Item1);
+            totQuantity += ratio[i].Item1;
+            if (i + 1 >= k) {
+                result = result == 0 ? (totQuantity * ratio[i].Item2) : Math.Min(result, totQuantity * ratio[i].Item2);
+                totQuantity -= heap.Dequeue();
             }
         }
-    }
-    return null;
+
+        return result;
     }
 }
