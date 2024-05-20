4f1967e3fb9490875b02df0a470045bb83189c35
Alex Dee <han73r@gmail.com>
2024-05-20 14:36:09 +0300

    Time: 64 ms (40.00%), Space: 40.27 MB (38.33%) - LeetHub

---
+++ 1863-sum-of-all-subset-xor-totals/1863-sum-of-all-subset-xor-totals.cs
@@ -0,0 +1,13 @@
+public class Solution {
+    public int SubsetXORSum(int[] nums) {
+        return GetSubsetXORSum(nums, 0, 0);
+    }
+    private int GetSubsetXORSum(int[] nums, int index, int xorTotal) {
+        if (index == nums.Length) {
+            return xorTotal;
+        }
+        int includeCurrent = GetSubsetXORSum(nums, index + 1, xorTotal ^ nums[index]);
+        int excludeCurrent = GetSubsetXORSum(nums, index + 1, xorTotal);
+        return includeCurrent + excludeCurrent;
+    }
+}
