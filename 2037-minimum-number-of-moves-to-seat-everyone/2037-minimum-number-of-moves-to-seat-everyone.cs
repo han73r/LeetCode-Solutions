5c3c2b635939194f3dffa4f0f2953e4e0fe1238a
Alex Dee <han73r@gmail.com>
2024-06-13 07:57:22 +0300

    Time: 69 ms (79.52%), Space: 42.49 MB (83.13%) - LeetHub

---
+++ 2037-minimum-number-of-moves-to-seat-everyone/2037-minimum-number-of-moves-to-seat-everyone.cs
@@ -0,0 +1,13 @@
+using System;
+
+public class Solution {
+    public int MinMovesToSeat(int[] seats, int[] students) {
+        int result = 0;
+        Array.Sort(seats);
+        Array.Sort(students);
+        for (int i = 0; i < students.Length; i++) {
+            result += Math.Abs(seats[i] - students[i]);
+        }
+        return result;
+    }
+}
