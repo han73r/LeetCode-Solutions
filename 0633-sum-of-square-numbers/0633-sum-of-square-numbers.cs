e2eda75e54f29de8c364849d12c667ddf9986592
Alex Dee <han73r@gmail.com>
2024-06-17 15:04:06 +0300

    Time: 49 ms (8.70%), Space: 28.99 MB (54.35%) - LeetHub

---
+++ 0633-sum-of-square-numbers/0633-sum-of-square-numbers.cs
@@ -0,0 +1,17 @@
+public class Solution {
+    public bool JudgeSquareSum(int c) {
+        for (int i = 2; i * i <= c; i++) {
+            if (c % i == 0) {
+                int count = 0;
+                while (c % i == 0) {
+                    count++;
+                    c /= i;
+                }
+                if (i % 4 == 3 && count % 2 != 0) {
+                    return false;
+                }
+            }
+        }
+        return c % 4 != 3;
+    }
+}
