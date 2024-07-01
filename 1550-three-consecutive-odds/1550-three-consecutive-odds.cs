22e3099d81d90e6e82c6a30b39cd91a44ad71444
Alex Dee <han73r@gmail.com>
2024-07-01 20:00:22 +0300

    Time: 68 ms (83.93%), Space: 43.08 MB (16.07%) - LeetHub

---
+++ 1550-three-consecutive-odds/1550-three-consecutive-odds.cs
@@ -0,0 +1,14 @@
+public class Solution {
+    public bool ThreeConsecutiveOdds(int[] arr) {
+        int counter = 0;
+        for (int i = 0; i < arr.Length; i++) {
+            if (arr[i] % 2 != 0) {
+                counter++;
+            } else {
+                counter = 0;
+            }
+            if (counter == 3) return true;
+        }
+        return false;
+    }
+}
