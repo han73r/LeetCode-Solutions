fb53cd4d618ad6a9a14ee9edf2701a342edebd38
Alex Dee <han73r@gmail.com>
2024-07-08 21:49:47 +0300

    Time: 29 ms (33.85%), Space: 26.77 MB (93.85%) - LeetHub

---
+++ 1823-find-the-winner-of-the-circular-game/1823-find-the-winner-of-the-circular-game.cs
@@ -0,0 +1,9 @@
+public class Solution {
+    public int FindTheWinner(int n, int k) {
+        int winner = 0;
+        for (int i = 1; i <= n; i++) {
+            winner = (winner + k) % i;
+        }
+        return winner + 1;
+    }
+}
