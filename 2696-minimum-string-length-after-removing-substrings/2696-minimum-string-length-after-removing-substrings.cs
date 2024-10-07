c57e4b4498bdd0f94a7e35cfe9f6c7957d352d49
Alex Dee <han73r@gmail.com>
2024-10-07 23:32:36 +0700

    Time: 45 ms (79.63%), Space: 41.57 MB (88.89%)

---
+++ 2696-minimum-string-length-after-removing-substrings/2696-minimum-string-length-after-removing-substrings.cs
@@ -0,0 +1,18 @@
+using System.Collections.Generic;
+
+public class Solution {
+    public int MinLength(string s) {
+        Stack<char> stack = new Stack<char>();
+        foreach (char c in s) {
+            if (stack.Count > 0) {
+                char top = stack.Peek();
+                if ((top == 'A' && c == 'B') || (top == 'C' && c == 'D')) {
+                    stack.Pop();
+                    continue;
+                }
+            }
+            stack.Push(c);
+        }
+        return stack.Count;
+    }
+}
