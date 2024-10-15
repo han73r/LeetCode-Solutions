0c282be96e3673a797831452816a458d117eb17f
Alex Dee <han73r@gmail.com>
2024-10-15 22:32:01 +0700

    Time: 68 ms (17.39%), Space: 45.67 MB (47.83%)

---
+++ 2938-separate-black-and-white-balls/2938-separate-black-and-white-balls.cs
@@ -0,0 +1,13 @@
+public class Solution {
+    public long MinimumSteps(string s) {
+        int whitePosition = 0;
+        long totalSwaps = 0;
+        for (int currentPos = 0; currentPos < s.Length; currentPos++) {
+            if (s[currentPos] == '0') {
+                totalSwaps += currentPos - whitePosition;
+                whitePosition++;
+            }
+        }
+        return totalSwaps;
+    }
+}
