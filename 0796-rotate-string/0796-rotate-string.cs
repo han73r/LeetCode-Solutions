f95ca4ade1c8a1248696797cf23effdfaa72829f
Alex Dee <han73r@gmail.com>
2024-11-03 22:12:36 +0700

    Time: 0 ms (100.00%), Space: 38.93 MB (88.93%)

---
+++ 0796-rotate-string/0796-rotate-string.cs
@@ -0,0 +1,7 @@
+public class Solution {
+    public bool RotateString(string s, string goal) {
+        if (s.Length != goal.Length) return false;
+        string doubled = s + s;
+        return doubled.Contains(goal);
+    }
+}
