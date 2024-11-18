cba4ed3373989c9287152e6411fdbe402ddb08f3
Alex Dee <han73r@gmail.com>
2024-11-18 12:01:24 +0700

    Time: 0 ms (100.00%), Space: 45.46 MB (100.00%)

--- 1652-defuse-the-bomb/1652-defuse-the-bomb.cs
+++ 1652-defuse-the-bomb/1652-defuse-the-bomb.cs
@@ -1,18 +1,19 @@
-// less memory
-public class Solution
-{
-    public int[] TwoSum(int[] nums, int target)
-    {
-    for (int i = 0; i < nums.Length; i++)
-    {
-        for (int j = i + 1; j < nums.Length; j++)
-        {
-            if (nums[i] + nums[j] == target)
-            {
-                return new int[] { i, j };
-            }
+public class Solution {
+    public int[] Decrypt(int[] code, int k) {
+        int n = code.Length;
+        int[] result = new int[n];
+        if (k == 0) return result;
+        int start = k > 0 ? 1 : k;
+        int end = k > 0 ? k : -1;
+        int sum = 0;
+        for (int i = start; i <= end; i++) {
+            sum += code[(i + n) % n];
         }
-    }
-    return null;
+        for (int i = 0; i < n; i++) {
+            result[i] = sum;
+            sum -= code[(start++ + n) % n];
+            sum += code[(++end + n) % n];
+        }
+        return result;
     }
 }
