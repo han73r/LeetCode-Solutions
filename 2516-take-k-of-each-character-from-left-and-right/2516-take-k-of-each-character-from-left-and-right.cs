1166556db6d69d264ed906afaa79badb20a9c07e
Alex Dee <han73r@gmail.com>
2024-11-21 11:19:11 +0700

    Time: 6 ms (100.00%), Space: 43.72 MB (55.56%)

--- 2516-take-k-of-each-character-from-left-and-right/2516-take-k-of-each-character-from-left-and-right.cs
+++ 2516-take-k-of-each-character-from-left-and-right/2516-take-k-of-each-character-from-left-and-right.cs
@@ -1,18 +1,35 @@
-// less memory
-public class Solution
-{
-    public int[] TwoSum(int[] nums, int target)
-    {
-    for (int i = 0; i < nums.Length; i++)
-    {
-        for (int j = i + 1; j < nums.Length; j++)
-        {
-            if (nums[i] + nums[j] == target)
-            {
-                return new int[] { i, j };
-            }
+using System;
+
+public class Solution {
+    public int TakeCharacters(string s, int k) {
+        int[] freq = new int[3];
+        int n = s.Length;
+
+        foreach (char c in s) {
+            freq[c - 'a']++;
         }
-    }
-    return null;
+
+        if (freq[0] < k || freq[1] < k || freq[2] < k) {
+            return -1;
+        }
+
+        int[] curr = new int[3];
+        int maxLen = 0;
+        int left = 0;
+
+        for (int right = 0; right < n; right++) {
+            curr[s[right] - 'a']++;
+
+            while (left <= right && (curr[0] > freq[0] - k ||
+                   curr[1] > freq[1] - k ||
+                   curr[2] > freq[2] - k)) {
+                curr[s[left] - 'a']--;
+                left++;
+            }
+
+            maxLen = Math.Max(maxLen, right - left + 1);
+        }
+
+        return n - maxLen;
     }
 }
