70fc419ec922561d990c5f11eb3eab69afde144d
Alex Dee <han73r@gmail.com>
2024-12-04 18:10:41 +0300

    Time: 3 ms (100.00%), Space: 52.43 MB (50.00%)

---
+++ 2825-make-string-a-subsequence-using-cyclic-increments/2825-make-string-a-subsequence-using-cyclic-increments.cs
@@ -0,0 +1,10 @@
+public class Solution {
+    public bool CanMakeSubsequence(string source, string target) {
+        int targetIdx = 0, targetLen = target.Length;
+        foreach (char currChar in source) {
+            if (targetIdx < targetLen && (target[targetIdx] - currChar + 26) % 26 <= 1)
+                targetIdx++;
+        }
+        return targetIdx == targetLen;
+    }
+}
