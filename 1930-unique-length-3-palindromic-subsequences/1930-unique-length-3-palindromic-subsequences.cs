790fc06d90f887da7a77886750b2a05a0544352c
Alex Dee <han73r@gmail.com>
2025-01-04 20:47:49 +0300

    Time: 155 ms (77.14%), Space: 55.28 MB (22.86%)

--- 1930-unique-length-3-palindromic-subsequences/1930-unique-length-3-palindromic-subsequences.cs
+++ 1930-unique-length-3-palindromic-subsequences/1930-unique-length-3-palindromic-subsequences.cs
@@ -1,18 +1,13 @@
-public class Solution
-{
-    public int CountPalindromicSubsequence(string s)
-    {
+public class Solution {
+    public int CountPalindromicSubsequence(string s) {
         HashSet<char> uniqueChars = new HashSet<char>(s);
         int count = uniqueChars
         .AsParallel()
-        .Sum(currentChar =>
-        {
+        .Sum(currentChar => {
             int l = s.IndexOf(currentChar);
             int r = s.LastIndexOf(currentChar);
             if (l != -1 && r != -1 && l < r)
-            {
                 return new HashSet<char>(s.Substring(l + 1, r - l - 1)).Count;
-            }
             return 0;
         });
         return count;
