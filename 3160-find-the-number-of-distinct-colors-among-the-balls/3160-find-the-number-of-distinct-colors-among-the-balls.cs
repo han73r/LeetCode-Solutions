387a7485707420b0304b14cb9ebff10f6fb85de5
Alex Dee <han73r@gmail.com>
2025-02-07 17:02:09 +0300

    Time: 19 ms (100.00%), Space: 99.03 MB (100.00%)

--- 3160-find-the-number-of-distinct-colors-among-the-balls/3160-find-the-number-of-distinct-colors-among-the-balls.cs
+++ 3160-find-the-number-of-distinct-colors-among-the-balls/3160-find-the-number-of-distinct-colors-among-the-balls.cs
@@ -1,18 +1,17 @@
-// less memory
-public class Solution
-{
-    public int[] TwoSum(int[] nums, int target)
-    {
-    for (int i = 0; i < nums.Length; i++)
-    {
-        for (int j = i + 1; j < nums.Length; j++)
-        {
-            if (nums[i] + nums[j] == target)
-            {
-                return new int[] { i, j };
-            }
+using System.Collections.Generic;
+
+public class Solution {
+    public int[] QueryResults(int limit, int[][] queries) {
+        int[] result = new int[queries.Length];
+        Dictionary<int, int> ballColor = new(queries.Length), colorCount = new(queries.Length);
+        for (int i = 0; i < queries.Length; i++) {
+            var q = queries[i];
+            int ball = q[0], newColor = q[1], prevColor = ballColor.GetValueOrDefault(ball);
+            if (prevColor > 0 && --colorCount[prevColor] == 0) colorCount.Remove(prevColor);
+            ballColor[ball] = newColor;
+            colorCount[newColor] = colorCount.GetValueOrDefault(newColor) + 1;
+            result[i] = colorCount.Count;
         }
-    }
-    return null;
+        return result;
     }
 }
