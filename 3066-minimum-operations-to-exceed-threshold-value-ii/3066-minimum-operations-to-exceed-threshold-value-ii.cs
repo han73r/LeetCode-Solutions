c5fd93e77886c82d1c9ac85a2da0cb6895e94006
Alex Dee <han73r@gmail.com>
2025-02-13 12:41:38 +0300

    Time: 151 ms (6.55%), Space: 52.94 MB (7.33%)

---
+++ 3066-minimum-operations-to-exceed-threshold-value-ii/3066-minimum-operations-to-exceed-threshold-value-ii.cs
@@ -0,0 +1,15 @@
+public class Solution {
+    public int RemoveDuplicates(int[] nums) {
+        int count = 0, current = 1;
+        for (int i = 1; i < nums.Length; i++) {
+            if (nums[i] != nums[i - 1]) {
+                count = 0;
+                nums[current++] = nums[i];
+            } else {
+                count++;
+                if (count <= 1) nums[current++] = nums[i];
+            }
+        }
+        return current;
+    }
+}
