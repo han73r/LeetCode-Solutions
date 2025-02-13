207bbdc19880856f6064ce6b2eff4a45d5b5bcd1
Alex Dee <han73r@gmail.com>
2025-02-13 12:43:08 +0300

    Time: 58 ms (100.00%), Space: 76.11 MB (100.00%)

--- 3066-minimum-operations-to-exceed-threshold-value-ii/3066-minimum-operations-to-exceed-threshold-value-ii.cs
+++ 3066-minimum-operations-to-exceed-threshold-value-ii/3066-minimum-operations-to-exceed-threshold-value-ii.cs
@@ -1,15 +1,28 @@
+using System.Collections.Generic;
+using System;
+
 public class Solution {
-    public int RemoveDuplicates(int[] nums) {
-        int count = 0, current = 1;
-        for (int i = 1; i < nums.Length; i++) {
-            if (nums[i] != nums[i - 1]) {
-                count = 0;
-                nums[current++] = nums[i];
-            } else {
-                count++;
-                if (count <= 1) nums[current++] = nums[i];
-            }
+    public int MinOperations(int[] nums, int k) {
+        int result = 0;
+        List<long> newNums = new List<long>();
+        Array.Sort(nums, (a, b) => b.CompareTo(a));
+        int i = nums.Length - 1;
+        int j = 0;
+        while (true) {
+            long a, b;
+            if (newNums.Count > 0 && j < newNums.Count && (i < 0 || newNums[j] < nums[i]))
+                a = newNums[j++];
+            else
+                a = nums[i--];
+            if (a >= k)
+                return result;
+            if (newNums.Count > 0 && j < newNums.Count && (i < 0 || newNums[j] < nums[i]))
+                b = newNums[j++];
+            else
+                b = nums[i--];
+            newNums.Add(a * 2 + b);
+            result++;
         }
-        return current;
+        return result;
     }
 }
