6b9799ce5925b8f388a8da90592b75989f8a0b6c
Alex Dee <han73r@gmail.com>
2025-02-28 12:58:42 +0300

    Time: 0 ms (100.00%), Space: 28.60 MB (71.06%)

--- 0070-climbing-stairs/0070-climbing-stairs-dp-memory-25.cs
+++ 0070-climbing-stairs/0070-climbing-stairs-dp-memory-25.cs
@@ -2,15 +2,12 @@ public class Solution {
     public int ClimbStairs(int n) {
         if (n == 0) return 0;
         if (n == 1) return 1;
-
-        int prev1 = 1, prev2 = 2;
-
+        int prev1 = 1, prev2 = 2, temp = 0;
         for (int i = 3; i <= n; i++) {
-            int result = prev1 + prev2;
+            temp = prev1 + prev2;
             prev1 = prev2;
-            prev2 = result;
+            prev2 = temp;
         }
-
         return prev2;
     }
 }
