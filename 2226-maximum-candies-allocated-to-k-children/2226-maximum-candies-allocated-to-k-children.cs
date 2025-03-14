b5c61dd827993db7ec2c708dae5523fbeea7307d
Alex Dee <han73r@gmail.com>
2025-03-14 13:06:46 +0300

    Time: 27 ms (90.63%), Space: 58.11 MB (21.88%)

---
+++ 2226-maximum-candies-allocated-to-k-children/2226-maximum-candies-allocated-to-k-children.cs
@@ -0,0 +1,26 @@
+using System.Linq;
+
+public class Solution {
+    public int MaximumCandies(int[] candies, long k) {
+        int left = 1, right = candies.Max();
+        int result = 0;
+
+        while (left <= right) {
+            int mid = (left + right) / 2;
+            long childrenCount = 0;
+
+            foreach (int candy in candies) {
+                childrenCount += candy / mid;
+            }
+
+            if (childrenCount >= k) {
+                result = mid;
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+
+        return result;
+    }
+}
