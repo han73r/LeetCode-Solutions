ab6d22b4559d492b5f195ddfefda4adbc6173ed9
Alex Dee <han73r@gmail.com>
2025-03-16 07:25:41 +0300

    Time: 14 ms (75.00%), Space: 54.78 MB (90.00%)

---
+++ 2594-minimum-time-to-repair-cars/2594-minimum-time-to-repair-cars.cs
@@ -0,0 +1,11 @@
+using System;
+
+public class Solution {
+    public long RepairCars(int[] ranks, int cars) {
+        long l = 0, h = (long)1E14, m, f;
+        for (int i; l < h; _ = f < cars ? l = ++m : h = m)
+            for (f = i = 0, m = (l + h) / 2;
+        f < cars && i < ranks.Length;) f += (long)Math.Sqrt(m / ranks[i++]);
+        return l;
+    }
+}
