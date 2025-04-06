946d12ff13fb11e1948bbd6b574d7b8cf33f1985
Alex Dee <han73r@gmail.com>
2025-04-06 16:57:03 +0300

    Time: 6 ms (36.05%), Space: 41.20 MB (50.00%)

--- 1863-sum-of-all-subset-xor-totals/1863-sum-of-all-subset-xor-totals2.cs
+++ 1863-sum-of-all-subset-xor-totals/1863-sum-of-all-subset-xor-totals2.cs
@@ -1,16 +1,11 @@
 public class Solution {
     public int SubsetXORSum(int[] nums) {
-        int n = nums.Length;
-        int sum = 0;
-        int subsets = 1 << n;
-        for (int i = 0; i < subsets; i++) {
-            int xor = 0;
-            for (int j = 0; j < n; j++) {
-                if ((i & (1 << j)) != 0)
-                    xor ^= nums[j];
-            }
-            sum += xor;
-        }
-        return sum;
+        return GetSubsetXORSum(nums, 0, 0);
+    }
+    private int GetSubsetXORSum(int[] nums, int index, int xorTotal) {
+        if (index == nums.Length) return xorTotal;
+        int includeCurrent = GetSubsetXORSum(nums, index + 1, xorTotal ^ nums[index]);
+        int excludeCurrent = GetSubsetXORSum(nums, index + 1, xorTotal);
+        return includeCurrent + excludeCurrent;
     }
 }
