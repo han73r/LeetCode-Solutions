3a86a2a2feda360666f7ad2b9f8b91eb3ef3b68c
Alex Dee <han73r@gmail.com>
2025-04-27 18:52:25 +0300

    Time: 194 ms (12.79%), Space: 77.56 MB (7.76%)

--- 2845-count-of-interesting-subarrays/2845-count-of-interesting-subarrays.cs
+++ 2845-count-of-interesting-subarrays/2845-count-of-interesting-subarrays.cs
@@ -1,18 +1,19 @@
-// less memory
-public class Solution
-{
-    public int[] TwoSum(int[] nums, int target)
-    {
-    for (int i = 0; i < nums.Length; i++)
-    {
-        for (int j = i + 1; j < nums.Length; j++)
-        {
-            if (nums[i] + nums[j] == target)
-            {
-                return new int[] { i, j };
-            }
+using System.Collections.Generic;
+
+public class Solution {
+    public long CountInterestingSubarrays(IList<int> nums, int modulo, int k) {
+        var map = new Dictionary<int, long> { [0] = 1 };
+        long res = 0;
+        int count = 0;
+
+        foreach (var num in nums) {
+            if (num % modulo == k) count++;
+            int curr = count % modulo;
+            int need = (curr - k + modulo) % modulo;
+            if (map.ContainsKey(need)) res += map[need];
+            if (!map.ContainsKey(curr)) map[curr] = 0;
+            map[curr]++;
         }
-    }
-    return null;
+        return res;
     }
 }
