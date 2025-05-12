bc12a8ff2579e4e30da3faaa930db8be38c76e8c
Alex Dee <han73r@gmail.com>
2025-05-12 18:16:38 +0300

    Time: 9 ms (65.63%), Space: 57.52 MB (18.75%)

---
+++ 2094-finding-3-digit-even-numbers/2094-finding-3-digit-even-numbers.cs
@@ -0,0 +1,23 @@
+using System.Collections.Generic;
+
+public class Solution {
+    public int[] FindEvenNumbers(int[] digits) {
+        List<int> result = new List<int>();
+        for (int i = 100; i < 1000; i += 2) {
+            int three = i / 100;
+            int two = (i / 10) % 10;
+            int one = i % 10;
+            List<int> temp = new List<int>(digits);
+
+            if (temp.Contains(three)) {
+                temp.Remove(three);
+                if (temp.Contains(two)) {
+                    temp.Remove(two);
+                    if (temp.Contains(one))
+                        result.Add(i);
+                }
+            }
+        }
+        return result.ToArray();
+    }
+}
