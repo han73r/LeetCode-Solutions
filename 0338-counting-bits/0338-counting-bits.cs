f75aacf37eb785fa664d132d6f0fcb2fe69f8acd
Alex Dee <han73r@gmail.com>
2025-05-22 15:18:25 +0300

    Time: 100 ms (100.00%), Space: 43.38 MB (70.71%)

---
+++ 0338-counting-bits/0338-counting-bits.cs
@@ -0,0 +1,8 @@
+public class Solution {
+    public int[] CountBits(int n) {
+        int[] dp = new int[n + 1];
+        for (int i = 0; i <= n; i++)
+            dp[i] = dp[i >> 1] + (i & 1);
+        return dp;
+    }
+}
