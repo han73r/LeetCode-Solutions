5b602a569766cfd7486a41960bb0feb65d8b10d1
Alex Dee <han73r@gmail.com>
2025-06-02 20:25:25 +0300

    Time: 4 ms (54.53%), Space: 48.80 MB (11.86%)

---
+++ 0135-candy/0135-candy.cs
@@ -0,0 +1,20 @@
+using System;
+
+public class Solution {
+    public int Candy(int[] ratings) {
+        int n = ratings.Length;
+        int[] left = new int[n], right = new int[n];
+        Array.Fill(left, 1);
+        Array.Fill(right, 1);
+        for (int i = 1; i < n; i++)
+            if (ratings[i] > ratings[i - 1])
+                left[i] = left[i - 1] + 1;
+        for (int i = n - 2; i >= 0; i--)
+            if (ratings[i] > ratings[i + 1])
+                right[i] = right[i + 1] + 1;
+        int count = 0;
+        for (int i = 0; i < n; i++)
+            count += Math.Max(left[i], right[i]);
+        return count;
+    }
+}
