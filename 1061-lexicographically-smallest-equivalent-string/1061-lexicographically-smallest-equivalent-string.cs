670509a7d4517c16e5e4a52dfbc60aa21233199f
Alex Dee <han73r@gmail.com>
2025-06-05 19:03:25 +0300

    Time: 1 ms (100.00%), Space: 41.23 MB (50.00%)

--- 1061-lexicographically-smallest-equivalent-string/1061-lexicographically-smallest-equivalent-string.cs
+++ 1061-lexicographically-smallest-equivalent-string/1061-lexicographically-smallest-equivalent-string.cs
@@ -1,18 +1,42 @@
-// less memory
-public class Solution
-{
-    public int[] TwoSum(int[] nums, int target)
-    {
-    for (int i = 0; i < nums.Length; i++)
-    {
-        for (int j = i + 1; j < nums.Length; j++)
-        {
-            if (nums[i] + nums[j] == target)
-            {
-                return new int[] { i, j };
-            }
-        }
+using System.Text;
+
+public class Solution {
+    public string SmallestEquivalentString(string A, string B, string S) {
+        var uf = new UnionFind(26);
+        for (int i = 0; i < A.Length; i++)
+            uf.Union(A[i] - 'a', B[i] - 'a');
+
+        var sb = new StringBuilder(S.Length);
+        foreach (var ch in S)
+            sb.Append((char)(uf.Find(ch - 'a') + 'a'));
+
+        return sb.ToString();
     }
-    return null;
+
+    private class UnionFind {
+        private readonly int[] parents;
+
+        public UnionFind(int count) {
+            this.parents = new int[count];
+            for (int i = 0; i < count; i++)
+                parents[i] = i;
+        }
+
+        public int Find(int index) {
+            if (parents[index] != index)
+                parents[index] = Find(parents[index]);
+            return parents[index];
+        }
+
+        public void Union(int index1, int index2) {
+            var pIndex1 = Find(index1);
+            var pIndex2 = Find(index2);
+
+            if (pIndex1 == pIndex2) return;
+            if (pIndex1 < pIndex2)
+                parents[pIndex2] = pIndex1;
+            else
+                parents[pIndex1] = pIndex2;
+        }
     }
 }
