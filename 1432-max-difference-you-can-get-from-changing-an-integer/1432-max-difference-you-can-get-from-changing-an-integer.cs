bdd17429fc53c27adea7e42e55cc730f76654229
Alex Dee <han73r@gmail.com>
2025-06-15 17:37:02 -0400

    Time: 0 ms (100.00%), Space: 29.55 MB (36.36%)

---
+++ 1432-max-difference-you-can-get-from-changing-an-integer/1432-max-difference-you-can-get-from-changing-an-integer.cs
@@ -0,0 +1,33 @@
+public class Solution {
+    public int MaxDiff(int num) {
+       string org = num.ToString();
+        string maxStr = org;
+        foreach (var item in org)
+        {
+            if (item != '9')
+            {
+                maxStr = org.Replace(item, '9');
+                break;
+            }
+        }
+
+        int maxNum = int.Parse(maxStr);
+        string minStr = org;
+        if (org[0] != '1')
+        {
+            minStr = org.Replace(org[0], '1');
+        } else {
+            for (int i = 1; i < org.Length; i++)
+            {
+                if (org[i] != '0' && org[i] != org[0])
+                {
+                    minStr = org.Replace(org[i], '0');
+                    break;
+                }
+            }
+        }
+        int minNum = int.Parse(minStr);
+
+        return maxNum - minNum;
+    }
+}
