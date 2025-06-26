290e3725422b42a2b0e05229a5e7be764804405e
Alex Dee <han73r@gmail.com>
2025-06-26 15:52:50 +0300

    Time: 3 ms (62.50%), Space: 40.48 MB (62.50%)

---
+++ 2311-longest-binary-subsequence-less-than-or-equal-to-k/2311-longest-binary-subsequence-less-than-or-equal-to-k.cs
@@ -0,0 +1,24 @@
+public class Solution {
+    public int LongestSubsequence(string s, int k) {
+        int count = 0;
+        int zeros = 0;
+        long value = 0;
+        int power = 0;
+        foreach (char ch in s)
+            if (ch == '0') zeros++;
+        for (int i = s.Length - 1; i >= 0; i--) {
+            if (s[i] == '1') {
+                if (power < 32 && value + (1L << power) <= k) {
+                    value += (1L << power);
+                    count++;
+                    power++;
+                } else {
+                    power++;
+                }
+            } else {
+                power++;
+            }
+        }
+        return count + zeros;
+    }
+}
