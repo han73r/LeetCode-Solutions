3fb54793b14a7039a5a600b7207467d0e339331a
Alex Dee <han73r@gmail.com>
2025-07-25 17:23:08 +0300

    Time: 9 ms (13.79%), Space: 44.65 MB (27.59%)

---
+++ 3487-maximum-unique-subarray-sum-after-deletion/3487-maximum-unique-subarray-sum-after-deletion.cs
@@ -0,0 +1,17 @@
+using System.Collections.Generic;
+using System.Linq;
+
+public class Solution {
+    public int MaxSum(int[] nums) {
+        HashSet<int> positiveNumsSet = new HashSet<int>();
+        foreach (int num in nums) {
+            if (num > 0) {
+                positiveNumsSet.Add(num);
+            }
+        }
+        if (positiveNumsSet.Count == 0) {
+            return nums.Max();
+        }
+        return positiveNumsSet.Sum();
+    }
+}
