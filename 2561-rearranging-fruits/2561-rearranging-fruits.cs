a3f3d3ece4e110983018f476afb52cb69b286e98
Alex Dee <han73r@gmail.com>
2025-08-02 10:53:20 +0300

    Time: 53 ms (25.00%), Space: 83.35 MB (50.00%)

---
+++ 2561-rearranging-fruits/2561-rearranging-fruits.cs
@@ -0,0 +1,34 @@
+using System.Collections.Generic;
+using System;
+
+public class Solution {
+    public long MinCost(int[] basket1, int[] basket2) {
+        var numFreq = new Dictionary<int, int>();
+        var result = 0L;
+        var min = int.MaxValue;
+        for (var i = 0; i < basket1.Length; i++) {
+            if (!numFreq.ContainsKey(basket1[i]))
+                numFreq[basket1[i]] = 1;
+            else
+                numFreq[basket1[i]]++;
+            if (!numFreq.ContainsKey(basket2[i]))
+                numFreq[basket2[i]] = -1;
+            else
+                numFreq[basket2[i]]--;
+        }
+        var moveList = new List<int>();
+        foreach (var num in numFreq) {
+            var n = num.Key;
+            var freq = num.Value;
+            if (freq % 2 != 0)
+                return -1;
+            for (var i = 0; i < Math.Abs(freq) / 2; i++)
+                moveList.Add(n);
+            min = Math.Min(min, n);
+        }
+        moveList.Sort();
+        for (var i = 0; i < moveList.Count / 2; i++)
+            result += Math.Min(moveList[i], min * 2);
+        return result;
+    }
+}
