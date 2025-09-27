c12d221f8041d556dcd0b36e48668fe7b48af88f
Alex Dee <han73r@gmail.com>
2025-09-27 14:54:24 +0300

    Time: 26 ms (97.37%), Space: 42.61 MB (15.79%)

---
+++ 0812-largest-triangle-area/0812-largest-triangle-area.cs
@@ -0,0 +1,20 @@
+using System;
+
+public class Solution {
+    public double LargestTriangleArea(int[][] points) {
+        double maxArea = 0.0;
+        int n = points.Length;
+        for (int i = 0; i < n; ++i) {
+            for (int j = i + 1; j < n; ++j) {
+                for (int k = j + 1; k < n; ++k) {
+                    int x1 = points[i][0], y1 = points[i][1];
+                    int x2 = points[j][0], y2 = points[j][1];
+                    int x3 = points[k][0], y3 = points[k][1];
+                    double area = 0.5 * Math.Abs(x1 * (y2 - y3) + x2 * (y3 - y1) + x3 * (y1 - y2));
+                    maxArea = Math.Max(maxArea, area);
+                }
+            }
+        }
+        return maxArea;
+    }
+}
