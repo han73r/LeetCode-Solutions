23cba087a90ec1c8190749c130a60a0c2af56c34
Alex Dee <han73r@gmail.com>
2025-09-30 14:32:35 +0300

    Time: 2 ms (83.33%), Space: 41.60 MB (83.33%)

---
+++ 1039-minimum-score-triangulation-of-polygon/1039-minimum-score-triangulation-of-polygon.cs
@@ -0,0 +1,22 @@
+using System;
+
+public class Solution {
+    public int MinScoreTriangulation(int[] values) {
+        int n = values.Length;
+        int[,] dp = new int[n, n];
+
+        for (int gap = 2; gap < n; gap++) {
+            for (int i = 0; i + gap < n; i++) {
+                int j = i + gap;
+                dp[i, j] = int.MaxValue;
+
+                for (int k = i + 1; k < j; k++) {
+                    int score = dp[i, k] + dp[k, j] + values[i] * values[k] * values[j];
+                    dp[i, j] = Math.Min(dp[i, j], score);
+                }
+            }
+        }
+
+        return dp[0, n - 1];
+    }
+}
