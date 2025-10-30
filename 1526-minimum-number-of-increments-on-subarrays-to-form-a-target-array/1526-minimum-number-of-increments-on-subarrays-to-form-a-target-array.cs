75110d8d597ca3949a4787268025e62c2b14bf34
Alex Dee <92275256+han73r@users.noreply.github.com>
2025-10-30 22:16:09 +0300

    Time: 2 ms (100.00%), Space: 60.71 MB (22.22%)

---
+++ 1526-minimum-number-of-increments-on-subarrays-to-form-a-target-array/1526-minimum-number-of-increments-on-subarrays-to-form-a-target-array.cs
@@ -0,0 +1,9 @@
+public class Solution {
+    public int MinNumberOperations(int[] target) {
+        int n = target.Length;
+        int ans = target[0];
+        for (int i = 1; i < n; ++i)
+            ans += Math.Max(target[i] - target[i - 1], 0);
+        return ans;
+    }
+}
