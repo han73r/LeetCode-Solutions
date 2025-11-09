2d6422f98dac9dccfcb136df3dc0d8880f5166cd
Alex Dee <92275256+han73r@users.noreply.github.com>
2025-11-09 16:46:57 +0300

    Time: 0 ms (100.00%), Space: 28.84 MB (95.65%)

---
+++ 2169-count-operations-to-obtain-zero/2169-count-operations-to-obtain-zero.cs
@@ -0,0 +1,11 @@
+public class Solution {
+    public int CountOperations(int num1, int num2) {
+        int res = 0;
+        while (num1 != 0 && num2 != 0) {
+            res += num1 / num2;
+            num1 %= num2;
+            (num1, num2) = (num2, num1);
+        }
+        return res;
+    }
+}
