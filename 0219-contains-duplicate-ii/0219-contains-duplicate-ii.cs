ec16aa9174b433ea78b39564c4967218333adfee
Alex Dee <92275256+han73r@users.noreply.github.com>
2025-12-17 16:14:48 +0100

    Time: 44 ms (32.40%), Space: 70.65 MB (37.29%)

---
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.cs
@@ -0,0 +1,11 @@
+public class Solution {
+    public bool ContainsNearbyDuplicate(int[] nums, int k) {
+        var set = new HashSet<int>();
+        for (int i = 0; i < nums.Length; i++) {
+            if (set.Contains(nums[i])) return true;
+            set.Add(nums[i]);
+            if (set.Count > k) set.Remove(nums[i - k]);
+        }
+        return false;
+    }
+}
