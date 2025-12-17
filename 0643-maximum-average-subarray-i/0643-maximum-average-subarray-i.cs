9ae25753a565de22e2156f6f6f088218790d8dd7
Alex Dee <92275256+han73r@users.noreply.github.com>
2025-12-17 15:50:16 +0100

    Time: 3 ms (82.77%), Space: 60.57 MB (29.60%)

---
+++ 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.cs
@@ -0,0 +1,13 @@
+public class Solution {
+    public double FindMaxAverage(int[] nums, int k) {
+        int sum = 0;
+        for (int i = 0; i < k; i++)
+            sum += nums[i];
+        int maxSum = sum;
+        for (int i = k; i < nums.Length; i++) {
+            sum += nums[i] - nums[i - k];
+            maxSum = Math.Max(maxSum, sum);
+        }
+        return (double)maxSum / k;
+    }
+}
