f2cada8cf47cac7dad55c3dee9a88a094d41dda8
Alex Dee <92275256+han73r@users.noreply.github.com>
2025-12-24 19:49:27 +0100

    Time: 7 ms (16.67%), Space: 45.51 MB (06.67%)

--- 3074-apple-redistribution-into-boxes/3074-apple-redistribution-into-boxes-priority-queue.cs
+++ 3074-apple-redistribution-into-boxes/3074-apple-redistribution-into-boxes-priority-queue.cs
@@ -1,15 +1,13 @@
 public class Solution {
     public int MinimumBoxes(int[] apple, int[] capacity) {
-        var sum = 0;
-        foreach (int i in apple) {
-            sum += i;
-        }
+        int sum = apple.Sum();
         var counter = 0;
+        var pq = new PriorityQueue<int, int>();
+        foreach (var cap in capacity)
+            pq.Enqueue(cap, -cap);
         while (sum > 0) {
-            var max = capacity.Max();
-            var index = Array.IndexOf(capacity, max);
+            var max = pq.Dequeue();
             sum -= max;
-            capacity[index] = -1;
             counter++;
         }
         return counter;
