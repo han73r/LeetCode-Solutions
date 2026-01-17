4facaf11ea58f761410d0ebde0b550cf95e9acaa
Alex Dee <92275256+han73r@users.noreply.github.com>
2026-01-17 09:34:54 +0100

    Time: 196 ms (33.33%), Space: 58.32 MB (66.67%)

---
+++ 3047-find-the-largest-area-of-square-inside-two-rectangles/3047-find-the-largest-area-of-square-inside-two-rectangles.cs
@@ -0,0 +1,21 @@
+public class Solution {
+    public long LargestSquareArea(int[][] bottomLeft, int[][] topRight) {
+      long maxSide=0;
+      int n=bottomLeft.Length;
+      for(int i=0;i<n;i++){
+        for(int j=i+1;j<n;j++){
+          int minX= Math.Max(bottomLeft[i][0],bottomLeft[j][0]);
+          int maxX= Math.Min(topRight[i][0],topRight[j][0]);
+          int minY= Math.Max(bottomLeft[i][1],bottomLeft[j][1]);
+          int maxY= Math.Min(topRight[i][1],topRight[j][1]);
+          if(minX<maxX && minY < maxY){
+            int width= maxX-minX;
+            int height= maxY-minY;
+            int side= Math.Min(width,height);
+            maxSide=Math.Max(maxSide, side);
+          }
+        }
+      }
+      return maxSide*maxSide;
+    }
+}
