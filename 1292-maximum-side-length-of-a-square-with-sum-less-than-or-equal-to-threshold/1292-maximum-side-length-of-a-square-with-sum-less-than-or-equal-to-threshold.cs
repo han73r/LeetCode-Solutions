a932b69842636030d78c313e3e56fe9156cfccc5
Alex Dee <92275256+han73r@users.noreply.github.com>
2026-01-19 15:04:12 +0100

    Time: 7 ms (81.82%), Space: 60.56 MB (09.09%)

---
+++ 1292-maximum-side-length-of-a-square-with-sum-less-than-or-equal-to-threshold/1292-maximum-side-length-of-a-square-with-sum-less-than-or-equal-to-threshold.cs
@@ -0,0 +1,32 @@
+public class Solution {
+    public int MaxSideLength(int[][] mat, int t) {
+        int m = mat.Length, n = mat[0].Length;
+        long[,] pre = new long[m+1,n+1];
+
+        for(int i=1;i<=m;i++){
+            long row=0;
+            for(int j=1;j<=n;j++){
+                row+=mat[i-1][j-1];
+                pre[i,j]=pre[i-1,j]+row;
+            }
+        }
+
+        bool Can(int len){
+            for(int i=len;i<=m;i++){
+                for(int j=len;j<=n;j++){
+                    long s = pre[i,j]-pre[i-len,j]-pre[i,j-len]+pre[i-len,j-len];
+                    if(s<=t) return true;
+                }
+            }
+            return false;
+        }
+
+        int lo=0, hi=Math.Min(m,n);
+        while(lo<hi){
+            int mid=(lo+hi+1)>>1;
+            if(Can(mid)) lo=mid;
+            else hi=mid-1;
+        }
+        return lo;
+    }
+}
