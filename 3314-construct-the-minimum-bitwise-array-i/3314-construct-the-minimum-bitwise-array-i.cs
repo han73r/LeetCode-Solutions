fda5f91a3ba58ec3c8aa9d58a67a5b9ee70862b3
Alex Dee <92275256+han73r@users.noreply.github.com>
2026-01-20 17:41:47 +0100

    Time: 0 ms (100.00%), Space: 49.80 MB (96.00%)

---
+++ 3314-construct-the-minimum-bitwise-array-i/3314-construct-the-minimum-bitwise-array-i.cs
@@ -0,0 +1,11 @@
+public class Solution {
+    public int[] MinBitwiseArray(IList<int> nums) {
+        int[] ans = new int[nums.Count];
+        for(int i = 0; i < nums.Count; i++) {
+            int n = nums[i];
+            if(n != 2) ans[i] = n - ((n + 1) & (-n - 1)) / 2;
+            else ans[i] = -1;
+        }  
+        return ans;
+    }
+}
