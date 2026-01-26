0f1a482d5921c7d44847ee639d24652acd6cca0c
Alex Dee <92275256+han73r@users.noreply.github.com>
2026-01-26 12:50:03 +0100

    Time: 49 ms (84.56%), Space: 64.37 MB (32.35%)

---
+++ 1200-minimum-absolute-difference/1200-minimum-absolute-difference.cs
@@ -0,0 +1,20 @@
+public class Solution {
+    public IList<IList<int>> MinimumAbsDifference(int[] arr) {
+        Array.Sort(arr);
+        IList<IList<int>> res = new List<IList<int>>();
+        int minDiff = int.MaxValue;
+
+        for (int i = 0; i < arr.Length - 1; i++) {
+            int diff = arr[i + 1] - arr[i];
+            if (diff < minDiff) {
+                minDiff = diff;
+                res.Clear();
+                res.Add(new List<int> { arr[i], arr[i + 1] });
+            } else if (diff == minDiff) {
+                res.Add(new List<int> { arr[i], arr[i + 1] });
+            }
+        }
+
+        return res;
+    }
+}
