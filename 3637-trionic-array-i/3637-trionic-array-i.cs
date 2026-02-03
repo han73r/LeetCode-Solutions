99d460345492c95ddd62953ee1b5c174a7667b36
Alex Dee <92275256+han73r@users.noreply.github.com>
2026-02-03 09:55:37 +0100

    Time: 0 ms (100.00%), Space: 45.99 MB (92.86%)

---
+++ 3637-trionic-array-i/3637-trionic-array-i.cs
@@ -0,0 +1,27 @@
+public class Solution {
+    public bool IsTrionic(int[] nums) {
+        int n = nums.Length;
+        int peak = n - 1;
+        int valley = 0;
+
+        for (int i = 0; i < n - 1; i++) {
+            if (peak == n - 1 && nums[i] >= nums[i + 1])
+                peak = i;
+            if (valley == 0 && nums[n - 1 - i] <= nums[n - 2 - i])
+                valley = n - 1 - i;
+            if (peak < valley)
+                return IsDecreasing(nums, peak, valley);
+        }
+
+        return false;
+    }
+
+    public bool IsDecreasing(int[] A, int a, int b) {
+        if (a == 0 || b == A.Length - 1)
+            return false;
+        for (int i = a; i < b; i++)
+            if (A[i] <= A[i + 1])
+                return false;
+        return true;
+    }
+}
