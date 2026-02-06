b3aa1a24835a871ee1e7c7379023f8b71b371b67
Alex Dee <92275256+han73r@users.noreply.github.com>
2026-02-06 08:51:48 +0100

    Time: 109 ms (80.00%), Space: 76.38 MB (20.00%)

---
+++ 3634-minimum-removals-to-balance-array/3634-minimum-removals-to-balance-array.cs
@@ -0,0 +1,16 @@
+public class Solution {
+    public int MinRemoval(int[] nums, int k) {
+        Array.Sort(nums);
+        int i = 0;
+        int maxLen = 0;
+        
+        for (int j = 0; j < nums.Length; j++) {
+            while ((long)nums[j] > (long)nums[i] * k) {
+                i++;
+            }
+            maxLen = Math.Max(maxLen, j - i + 1);
+        }
+        
+        return nums.Length - maxLen;
+    }
+}
